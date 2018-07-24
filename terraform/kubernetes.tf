91166c82ba32ce69cc813fdf7b0ad5cdd4097721
Lisa Seelye <lisa@users.noreply.github.com>
2018-07-24 15:33:13 -0400

    basic support to deploy a container to gke

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -8,16 +8,29 @@ variable "gke_node_count" {
   description = "Initial node count for the GKE cluster"
 }
 
+variable "gke_node_type" {
+  description = "What kind of GCE node type to use for the GKE node pools"
+}
+
 resource "google_container_cluster" "primary" {
   name = "cluster"
-  initial_node_count = 3
+  initial_node_count = "${var.gke_node_count}"
   network = "${google_compute_network.primary_network.self_link}"  
   
   node_config {
-    disk_size_gb = "10"
-    machine_type = "f1-micro"
+    disk_size_gb = "${var.gke_node_disk_size}"
+    machine_type = "${var.gke_node_type}"
     preemptible = true
     tags = [ "gke" ]
+    
+    oauth_scopes = [
+      "compute-rw",
+      "storage-ro",
+      "logging-write",
+      "monitoring",
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+    ]
   }
   
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,55 @@
-variable "project_name" {
+variable "project_id" {
   description = "Name of the project"
+}
+
+variable "gcp_region" {
+  description = "GCP Region to use"
+}
+
+variable "gcp_zone" {
+  description = "GCP Zone to use"
+}
+
+variable "google_project_name" {
+  description = "Human name of the project"
 }
 
 provider "google" {
   credentials = "${file("account.json")}"
-  project = "${var.project_name}"
-  region = "us-east1"
-  zone = "us-east1-b"
+  project = "${var.project_id}"
+  region = "${var.gcp_region}"
+  zone = "${var.gcp_zone}"
+}
+
+resource "google_project_services" "services" {
+  project = "${var.project_id}"
+  services = [
+    "bigquery-json.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "compute.googleapis.com",
+    "container.googleapis.com",
+    "containerregistry.googleapis.com",
+    "deploymentmanager.googleapis.com",
+    "iam.googleapis.com",
+    "logging.googleapis.com",
+    "oslogin.googleapis.com",
+    "pubsub.googleapis.com",
+    "replicapool.googleapis.com",
+    "replicapoolupdater.googleapis.com",
+    "resourceviews.googleapis.com",
+    "serviceusage.googleapis.com",
+    "storage-api.googleapis.com",
+  ]
+}
+
+resource "google_project" "project" {
+  name = "${var.google_project_name}"
+  project_id = "${var.project_id}"
+}
+
+data "google_project" "project" {}
+
+output "project_number" {
+  value = "${data.google_project.project.number}"
 }
