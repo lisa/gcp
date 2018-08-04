09d5f3573e349a23f1c9d98b124b2454f5cabda3
Lisa Seelye <lisa@users.noreply.github.com>
2018-08-04 14:31:20 -0400

    Reserve a global public IP for Ingress

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,3 +1,12 @@
 resource "google_compute_network" "primary_network" {
   name = "primary-network"
 }
+
+resource "google_compute_global_address" "public_app_ip" {
+  name = "public-ip"
+}
+
+
+output "public_app_ip" {
+  value = "${google_compute_global_address.public_app_ip.address}"
+}
